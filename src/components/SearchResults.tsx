1d05c690727b6cc10fef3f7e51f463987b258b2a
khirthane <khirthane@gmail.com>
2024-02-24 11:11:39 +0100

    Updated the Search Results Page

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -19,9 +19,9 @@ const SearchResults = ({ plugins }: SearchResultsProps) => {
     <>
       {results.map((plugin, index) => (
         <div className='card' key={index}>
-          <div className='flex justify-between flex-col'>
-            <div className='cardName text-xl'>{plugin.name}</div>
-            <div className='cardTitle text-s'>{plugin.description}</div>
+          <div className='packageName text-xl'>{plugin.name}</div>
+          <div className='cardTitle text-s'>{plugin.description}</div>
+          <div className='flex justify-between'>
             <div className='releaseDate text-xs opacity-70'>
               Latest Release {plugin.latest_release_number}
               {' - '}
@@ -30,11 +30,6 @@ const SearchResults = ({ plugins }: SearchResultsProps) => {
             <div className='stars flex gap-1 text-xs opacity-70'>
               <img src='/assets/icons/star.svg' width={12} />
               {readableClount(plugin.stars as number)} Stars
-            </div>
-          </div>
-          <div className='download'>
-            <div className=''>
-              <a href={plugin.latest_download_url as string}>Download</a>
             </div>
           </div>
         </div>
